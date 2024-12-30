8cbda1ce46792ad24fbff13d71b97481df678183
nsgamliel <nsgamliel@gmail.com>
2024-12-30 01:55:48 -0500

    change origin rules

--- server.js
+++ server.js
@@ -20,11 +20,11 @@ const gamesRef = collection(db, "games");
 const PORT = 3000;
 
 const httpServer = createServer();
-const io = new Server(httpServer, {
+const io = new Server(httpServer);/*, {
 	cors: {
-		origin: "http://localhost:5173"
+		origin: "*"
 	}
-});
+});*/
 
 
 const chars = 'abcdefghijklmnopqrstuvqxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
